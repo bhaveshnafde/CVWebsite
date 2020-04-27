50f60fadb12f1575e24e8c24c54664bdbd74cdec
bhaveshnafde <36959638+bhaveshnafde@users.noreply.github.com>
2020-04-27 14:30:09 +0530

    s

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'info@yourdomain.com'; // Replace it with Your Hosting Admin email. REQUIRED!
-$sendTo = 'naphadebhavesh99'; // Replace it with Your email. REQUIRED!
+$from = 'info@bhaveshnaphade.in'; // Replace it with Your Hosting Admin email. REQUIRED!
+$sendTo = 'naphadebhavesh99@gmail.com'; // Replace it with Your email. REQUIRED!
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
@@ -12,7 +12,7 @@ $errorMessage = 'There was an error while submitting the form. Please try again 
 
 if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
     //your site secret key
-    $secret = '6LcnuOsUAAAAACqfPxH8Ip6ok_KkSvJw-IJ7YClM';
+    $secret = '6LdE4u4UAAAAAIFT0UUdsKwFbcmfugTLGiBVNJim';
     //get verify response data
 
     $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
